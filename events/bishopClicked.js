0c7a1544c9464d525bc4bd13909ed486e8e6c52c
HRK-Stack <hrk.stack@gmail.com>
2025-07-07 11:16:09 +0530

    Added Bishop Movement Functionality

--- events/bishopClicked.js
+++ events/bishopClicked.js
@@ -1,8 +1,8 @@
-import { selectedSqr, sqrData } from "../utils/G_Constants.js";
+import { highlightInDangerSquare, highlightSquare } from "../js/render/render.js";
+import { selectedSqr, setSelectedSqr, sqrData } from "../utils/G_Constants.js";
+import { sqrMap } from "../utils/map.js";
 
 export function whiteBishopClicked(sqr){
-    console.log(sqr);
-
     const currPos = sqr.id;
     const col = currPos[0]; // a,b,c .. h
     const row =  parseInt(currPos[1]); //1,2 .. 8
@@ -28,10 +28,71 @@ export function whiteBishopClicked(sqr){
 
             const id  = targetCol + targetRow;
 
-            // const square = 
+            const square = sqrMap[id];
+            if (!square) break; 
+
+            if(square.piece == null){
+                square.highlight = true;
+            }
+            else{
+                if(square.piece.name.includes("BLACK")){
+                    square.highlight = true;
+                    square.inDanger = true;
+                }
+                break;
+            }
         }
     })
     
+    setSelectedSqr(sqr);
+    highlightSquare();
+    highlightInDangerSquare();
     
+}
+
+export function blackBishopClicked(sqr){
+    const currPos = sqr.id;
+    const col = currPos[0]; // a,b,c .. h
+    const row =  parseInt(currPos[1]); //1,2 .. 8
+
+    const directions = [
+        {dcol:1 ,drow:1},
+        {dcol:-1 ,drow:1},
+        {dcol:1 ,drow:-1},
+        {dcol:-1 ,drow:-1},
+    ]
+
+    directions.forEach(({dcol,drow}) =>{
+        let colCode = col.charCodeAt(0);
+        let targetRow = row;
+        
+        while (true) {
+            colCode += dcol;
+            targetRow += drow;
+
+            let targetCol = String.fromCharCode(colCode);
+
+            if(targetCol < 'a' || targetCol > 'h' || targetRow < 1 || targetRow >8) break;
+
+            const id  = targetCol + targetRow;
+
+            const square = sqrMap[id];
+
+            if(square.piece == null){
+                square.highlight = true;
+            }
+            else{
+                if(square.piece.name.includes("WHITE")){
+                    square.highlight = true;
+                    square.inDanger = true;
+                }
+                break;
+            }
+        }
+    })
+    
+    setSelectedSqr(sqr);
+    highlightSquare();
+    highlightInDangerSquare();
     
 }
--- events/global.js
+++ events/global.js
@@ -1,7 +1,7 @@
 import { clearHighlight, clearInDangerSquare, movePieceTo, selfHighlight } from "../js/render/render.js";
 import { ROOT_DIV, selectedSqr, sqrData } from "../utils/G_Constants.js";
 import { sqrMap } from "../utils/map.js";
-import { whiteBishopClicked } from "./bishopClicked.js";
+import { blackBishopClicked, whiteBishopClicked } from "./bishopClicked.js";
 import {  blackPawnClicked, whitePawnClicked } from "./pawnClicked.js";
 
 
@@ -17,14 +17,15 @@ export function globalEvent(){
                 clearInDangerSquare();
             }else{
                 clearInDangerSquare();
+                clearHighlight();
                 selfHighlight(clickedId); // highlight clicked piece
 
                 if(sqr.piece.name == "WHITE_PAWN") whitePawnClicked(sqr);
                 else if(sqr.piece.name == "BLACK_PAWN") blackPawnClicked(sqr);
                 else if(sqr.piece.name == "WHITE_BISHOP") whiteBishopClicked(sqr);
-                
-                // if(sqr.piece.name == "WHITE_ROOK") whiteRookClicked(sqr);
-                // if(sqr.piece.name == "BLACK_ROOK") blackRookClicked(sqr);
+                else if(sqr.piece.name == "BLACK_BISHOP") blackBishopClicked(sqr);
+                // else if(sqr.piece.name == "WHITE_ROOK") whiteRookClicked(sqr);
+                // else if(sqr.piece.name == "BLACK_ROOK") blackRookClicked(sqr);
             } 
         }   
         if(event.target.localName == 'div' && event.target.id !== "root"){
